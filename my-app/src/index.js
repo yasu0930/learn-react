867dc00c48b61eb895334e89ef486817c08f532f
yasu0930 <yasuhon93@gmail.com>
2021-08-26 21:45:16 +0900

    hook

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -42,11 +42,13 @@ const Board = ({ squares, onClick }) => {
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
+      
       <div className="board-row">
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
+
       <div className="board-row">
         {renderSquare(6)}
         {renderSquare(7)}
