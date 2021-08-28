8a72bdd9695a428ea374e7bb1b9462d1fb9a72d5
yasu0930 <yasuhon93@gmail.com>
2021-08-28 16:46:49 +0900

    dispaly events refactor and remove event button

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useReducer } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Event from './Event'
 import reducer from '../reducers'
 
 const App = () => {
@@ -46,9 +47,12 @@ const App = () => {
               <th>ID</th>
               <th>タイトル</th>
               <th>ボディー</th>
+              <th></th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+          </tbody>
         </table>
       </form>
     </div>
