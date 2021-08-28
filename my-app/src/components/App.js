845b768d77776b54f69d92a2ab1fff74d697bcd4
yasu0930 <yasuhon93@gmail.com>
2021-08-28 17:28:30 +0900

    implement all events deletion feature

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -23,6 +23,12 @@ const App = () => {
     setBody('')
   }
 
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
+    if(result) dispatch({ type: 'DELETE_ALL_EVENTS' })
+  }
+
   return(
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -37,8 +43,8 @@ const App = () => {
           <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
         </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent} disabled={title === '' || body === ''}>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
         
         <h4>イベント一覧</h4>
         <table className="table table-hover">
@@ -51,7 +57,7 @@ const App = () => {
             </tr>
           </thead>
           <tbody>
-            { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+            { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
           </tbody>
         </table>
       </form>
--- my-app/src/components/Event.js
+++ my-app/src/components/Event.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Event = (event, dispatch) => {
+const Event = ({event, dispatch}) => {
   const id = event.id
   const handleClickDeleteButton = () => {
-    dispatch({ type: 'DELETE_EVENT', id})
+    const result = window.confirm(`イベント(id=${id})を本当に削除しても良いですか？`)
+    if(result) dispatch({ type: 'DELETE_EVENT', id})
   }
   
   return (
